9135f7fc44193df2206f8ff5976f91b29e0f97a2
DoomSwarmed <joshuar03@live.co.uk>
2020-10-02 14:22:52 +0100

    Mostly UI

---
+++ Assets/scripts/StopWatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour {
+    public float seconds;
+    public float minutes;
+    public Text textBox;
+
+    bool timerActive = true;
+
+	// Use this for initialization
+	void Start () {
+        textBox.text = seconds.ToString("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timerActive){
+            seconds += (Time.deltaTime % 60);
+            minutes += (Time.deltaTime / 60);
+            if (seconds > 60)
+            {
+                seconds = 0;
+            }
+            int secc = (int)seconds;
+            int mins = (int)minutes;
+            textBox.text = "TIMER: " + mins.ToString("00") + ":" + secc.ToString("00");
+
+        }
+	}
+}
---
+++ Assets/scripts/turnScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class turnScript : MonoBehaviour
+{
+    public bool oturn;
+    public bool xturn;
+    public Text otext;
+    public Text xtext;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        oturn = true;
+      otext.text = "O's\nturn";
+        xturn = false;
+        xtext.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (oturn == true) {
+            otext.text = "O's\nturn";
+        }
+        else {
+            xtext.text = "X's\nturn";
+        }
+        
+    }
+
+    public void ChangeTurns()
+    {
+        if (oturn == true) {
+            oturn = false;
+            otext.text = "";
+            Debug.Log("djsd");
+        }
+        else {
+            oturn = true;
+            otext.text = "O's\nturn";
+            xtext.text = "";
+
+
+        }
+    }
+}
